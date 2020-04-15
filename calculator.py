5b37cc97fb5110aa681de9a3ff9f88350f40e92d
RuslanPerm <manfuckschool@mail.ru>
2020-04-15 12:57:29 +0500

    Добавлена проверка гендера

--- calculator.py
+++ calculator.py
@@ -1,4 +1,8 @@
 print('Я помогу вам узнать ваш индекс массы тела и скажу пару слов')
 height = float(input('Введите ваш рост: '))
 weight = float(input('Введите вашу массу: '))
-index = 0
+index, gender = 0, input('Вы man or woman: ')
+if gender == 'man':
+    pass
+elif gender == 'woman':
+    pass
