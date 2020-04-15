ae275881d7ed14eab64fb9aff3499124df106996
RuslanPerm <manfuckschool@mail.ru>
2020-04-15 13:15:30 +0500

    Добавлены комментарии по поводу массы

--- calculator.py
+++ calculator.py
@@ -2,3 +2,11 @@ print('Я помогу вам узнать ваш индекс массы тел
 height = float(input('Введите ваш рост: '))
 weight = float(input('Введите вашу массу: '))
 index = 0
+if index < 18.5:
+    print('Деффицит массы')
+elif (index < 30) and (index > 25):
+    print('избыток массы')
+elif index > 30:
+    print('ожирение, чтобы узнать степень обратитесь к диетологу')
+else:
+    print('норма')
